baf0bf3c604065584f75efee64b492620b05210b
Parseee <81364541+Parseee@users.noreply.github.com>
2023-12-09 10:17:43 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ilababakov/Documents/code/study_solutions/prac/Matreshkaprak4
-# Build directory: /Users/ilababakov/Documents/code/study_solutions/prac/Matreshkaprak4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
