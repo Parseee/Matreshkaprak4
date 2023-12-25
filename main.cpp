06c33028f9a3243b19078df3e3d405728cc25de2
ArtZap <zarchy888@gmail.com>
2023-12-25 10:13:24 +0300

    ....

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
     setlocale(LC_ALL, "en-US.UTF-8");
     std::vector < Token > lexes;
     try {
-        lexes = lex_an(read_file("../materials/prgrm.rus"));
+        lexes = lex_an(read_file("materials/prgrm.rus"));
     } catch (std::vector < std::invalid_argument > e) {
         for (int i = 0; i < e.size(); i++)
             std::cerr << "exception " << e[i].what() << "\n\n";
@@ -26,6 +26,8 @@ int main() {
     } catch (std::logic_error e) {
         std::cerr << "Exception " << e.what() << std::endl;
         return 0;
+    }catch(const std::overflow_error& e){
+        std::cerr<<"Unknown exception"<<std::endl;
     }
     std::cout << "Syntax analysis complete sucsessfully\n";
 }
